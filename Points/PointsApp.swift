27df2f137dbbc6b49e25f1781dc802a157d18e95
Alexander Khitev <alexsanderkhitev@gmail.com>
2022-09-19 13:43:30 +0400

    Initial commit

---
+++ Points/PointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointsApp.swift
+//  Points
+//
+//  Created by Alexander Khitev on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
