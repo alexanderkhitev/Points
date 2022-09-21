6428817d820f8abd6d6572b7faa7c876990a07ac
Alexander Khitev <alexsanderkhitev@gmail.com>
2022-09-21 14:58:38 +0400

    wip 0

--- Points/UI/Entry/Detail/Rows/DetailPointsListGraphRow.swift
+++ Points/UI/Entry/Detail/Rows/DetailPointsListGraphRow.swift
@@ -12,14 +12,20 @@ struct DetailPointsListGraphRow: View {
     let pointsHUB: PointsHUB
 
     var body: some View {
-        Chart(pointsHUB.sortedPoints) { point in
-            LineMark(x: .value("x", point.x),
-                     y: .value("y", point.y))
-            .lineStyle(.init(lineWidth: 3,
-                             lineCap: .round,
-                             lineJoin: .round))
-            .interpolationMethod(.catmullRom)
-            .symbol(.circle)
+        GeometryReader { geometry in
+            ScrollView(.horizontal) {
+                Chart(pointsHUB.sortedPoints) { point in
+                    LineMark(x: .value("x", point.x),
+                             y: .value("y", point.y))
+                    .lineStyle(.init(lineWidth: 3,
+                                     lineCap: .round,
+                                     lineJoin: .round))
+                    .interpolationMethod(.catmullRom)
+                    .symbol(.circle)
+                }
+                .frame(minWidth: max(CGFloat(pointsHUB.sortedPoints.count) * 10, geometry.size.width))
+                .padding([.vertical, .trailing])
+            }
         }
         .frame(height: 300)
     }
